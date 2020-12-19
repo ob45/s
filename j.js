c3bb5db3e4d4cea5b5a555ce9c106a1f4664ef37
Tiberiu-Arthur Nowotny <thundersocks69@gmail.com>
2020-12-19 19:28:46 +0100

    Added XSS

---
+++ j.js
@@ -0,0 +1,44 @@
+//start_me_senpai()
+//lePrank();
+
+
+
+lel();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function start_me_senpai(){
+alert("You have been gnomed :DDD");
+window.open("https://google.at/","_self");
+}
+
+function lePrank(){
+
+
+    alert("I have a dream");
+    alert("That one day");
+    alert("I'll be able to fly");
+    alert("to sing");
+    alert("to explore the universe");
+    alert("And to escape this nightmare");
+
+
+
+}
+
+function lel(){
+alert("Due to low ad revenue our cdn server has been shut down.\n Messages must now be sent trough 9gag.com");
+window.open("https://9gag.com","_self");
+
+}
